8653974816c1881cb1be2f9e2118a73bd86b35d6
Ed <ehartley38@gmail.com>
2024-09-23 19:50:51 +0100

    1000x1000 pixel canvas implemented

--- r-place-node/utils/createSampleCanvas.js
+++ r-place-node/utils/createSampleCanvas.js
@@ -1,26 +1,29 @@
 import Redis from "ioredis";
 
-const canvasWidth = process.env.CANVAS_WIDTH || 100
+const canvasWidth = process.env.CANVAS_WIDTH || 1000;
 
 const redis = new Redis({});
 
 const createCanvas = async () => {
+  console.log("Creating sample canvas of width", canvasWidth);
+
   let cnt = 0;
 
   for (let x = 0; x < canvasWidth; x++) {
     for (let y = 0; y < canvasWidth; y++) {
-      const randomColour = Math.floor(Math.random() * 31)
+      const randomColour = Math.floor(Math.random() * 31);
 
       await setPixelColour(x, y, randomColour);
     }
+    if (x % 100 == 0) console.log(x);
   }
 
   console.log("Complete");
 };
 
 const setPixelColour = async (x, y, colour) => {
-  const offset = (y * canvasWidth + x);
-  await redis.bitfield("canvas_bitmap", "SET", "u8", `#${offset}`, colour)
+  const offset = y * canvasWidth + x;
+  await redis.bitfield("canvas_bitmap", "SET", "u8", `#${offset}`, colour);
 };
 
 try {
