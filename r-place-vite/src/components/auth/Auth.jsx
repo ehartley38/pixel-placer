ce11d477c96c41f24164c30212a01994305e220d
Ed <ehartley38@gmail.com>
2024-09-24 20:27:55 +0100

    Login redirect if session valid

--- r-place-vite/src/components/auth/Auth.jsx
+++ r-place-vite/src/components/auth/Auth.jsx
@@ -1,9 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { supabase } from "../../services/supabaseClient";
+import { useNavigate } from "react-router-dom";
 
-export default function Auth() {
+export default function Auth({ session }) {
   const [loading, setLoading] = useState(false);
   const [email, setEmail] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (session) {
+      navigate("/");
+    }
+  }, [session, navigate]);
 
   const handleLogin = async (event) => {
     event.preventDefault();
@@ -20,30 +28,30 @@ export default function Auth() {
   };
 
   return (
-      <div className="row flex flex-center text-black">
-        <div className="col-6 form-widget">
-          <h1 className="header">R-Place</h1>
-          <p className="description">
-            Sign in via magic link with your email below
-          </p>
-          <form className="form-widget" onSubmit={handleLogin}>
-            <div>
-              <input
-                className="inputField"
-                type="email"
-                placeholder="Your email"
-                value={email}
-                required={true}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            <div>
-              <button className={"button block"} disabled={loading}>
-                {loading ? <span>Loading</span> : <span>Send magic link</span>}
-              </button>
-            </div>
-          </form>
-        </div>
+    <div className="row flex flex-center text-black">
+      <div className="col-6 form-widget">
+        <h1 className="header">R-Place</h1>
+        <p className="description">
+          Sign in via magic link with your email below
+        </p>
+        <form className="form-widget" onSubmit={handleLogin}>
+          <div>
+            <input
+              className="inputField"
+              type="email"
+              placeholder="Your email"
+              value={email}
+              required={true}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div>
+            <button className={"button block"} disabled={loading}>
+              {loading ? <span>Loading</span> : <span>Send magic link</span>}
+            </button>
+          </div>
+        </form>
       </div>
+    </div>
   );
 }
