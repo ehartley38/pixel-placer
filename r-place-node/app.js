004614d3162786b2be90d029efabb4f190e361d1
Ed <ehartley38@gmail.com>
2024-11-19 20:32:12 +0000

    Allowed origins test

--- r-place-node/app.js
+++ r-place-node/app.js
@@ -3,10 +3,14 @@ import cors from "cors";
 import mainRouter from "./controllers/main.js";
 import { corsOptions } from "./utils/corsOptions.js";
 import { initialiseRedis } from "./utils/initialiseRedis.js";
+import { config } from "./utils/config.js"
 
 const app = express();
 
 initialiseRedis();
+
+console.log("Allowed origins HASB: ", config.allowedOrigins);
+
 
 app.use(cors(corsOptions));
 app.use(express.json());
