1ecd185d74ca2f315aa98f6b0c7c3c398d6b7448
Ed <ehartley38@gmail.com>
2024-11-20 19:14:05 +0000

    Print origin

--- r-place-node/utils/corsOptions.js
+++ r-place-node/utils/corsOptions.js
@@ -5,6 +5,8 @@ import { config } from "./config.js";
 export const corsOptions = {
   
     origin: (origin, callback) => {
+      console.log("Origin:", origin);
+      
       if (config.allowedOrigins.indexOf(origin) !== -1 || !origin) {
         callback(null, true);
       } else {
